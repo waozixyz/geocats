23c8b8c2e5367245b90319cd43c3759c56f42a85
wolfi <wolfi@disroot.org>
2021-07-04 17:44:39 +0200

    add letter + present

--- Scenes/1_CatCradle/Present.gd
+++ Scenes/1_CatCradle/Present.gd
@@ -1,12 +1,20 @@
 extends Area2D
 
+onready var letter = get_node("../../Letter/Content")
 
 func _ready():
-	connect("body_entered", self, "_on_body_enter")
+	set_process_input(true)
+
+func toggle(boolean):
+	if boolean:
+		return false
+	else:
+		return true
 
 func _input(event):
-	if Input.is_action_pressed("interact"):
+	if Input.is_action_just_pressed("interact"):
+		if  get_parent().visible || letter.visible:
+			letter.visible = toggle(letter.visible)
+
 		get_parent().visible = false
-func _on_body_enter(body):
-	if body.name == "Player":
 		print("hi")
