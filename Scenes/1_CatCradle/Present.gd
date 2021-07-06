395243ff7b95b4df1b501b8b046ee8a4c5146c20
wolfi <wolfi@disroot.org>
2021-07-06 17:19:37 +0200

    fix catcradle letter opening

--- Scenes/1_CatCradle/Present.gd
+++ Scenes/1_CatCradle/Present.gd
@@ -2,9 +2,21 @@ extends Area2D
 
 onready var letter = get_node("../../Letter/Content")
 
+var touching = false
+
 func _ready():
 	set_process_input(true)
-
+	connect("body_entered", self, "_on_body_entered")
+	connect("body_exited", self, "_on_body_exited")
+	
+func _on_body_entered(body):
+	if body.name == "Player":
+		touching = true
+	
+func _on_body_exited(body):
+	if body.name == "Player":
+		touching = false
+	
 func toggle(boolean):
 	if boolean:
 		return false
@@ -12,7 +24,7 @@ func toggle(boolean):
 		return true
 
 func _input(event):
-	if Input.is_action_just_pressed("interact"):
+	if Input.is_action_just_pressed("interact") && touching:
 		if  get_parent().visible || letter.visible:
 			letter.visible = toggle(letter.visible)
 
