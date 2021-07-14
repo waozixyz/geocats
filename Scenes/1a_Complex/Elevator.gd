a06a5954c599001388b87a6804a5e8946f6336ca
wolfi <wolfi@disroot.org>
2021-07-14 13:12:33 +0200

    add elevator functions

--- Scenes/1a_Complex/Elevator.gd
+++ Scenes/1a_Complex/Elevator.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 onready var button = $Button
 
+onready var player =  get_tree().get_current_scene().get_node("Player")
+
 func _ready():
 	connect("body_entered", self, "_on_body_entered")
 	connect("body_exited", self, "_on_body_exited")
@@ -18,4 +20,9 @@ func _on_body_exited(body):
 func _input(event):
 	if Input.is_action_just_pressed("interact") && button.visible == true:
 		button.visible = false
-
+		if get_parent().name == "UpElevator":
+			player.position.x = 2400
+			player.position.y = 1600
+		if get_parent().name == "DownElevator":
+			player.position.x = 1100
+			player.position.y = 300
