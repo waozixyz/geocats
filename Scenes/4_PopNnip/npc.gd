19a1e7f13f8f3324f9cd1b4e06f8b0fc7e58deca
wolfi <wolfi@disroot.org>
2021-07-22 21:56:20 +0200

    improve arcade

---
+++ Scenes/4_PopNnip/npc.gd
@@ -0,0 +1,12 @@
+extends AnimatedSprite
+var f = 0.002
+var elapsed = 0
+func _physics_process(delta):
+	var ticks = OS.get_ticks_msec() * f
+	frame = int(ticks) % 3
+	if OS.get_ticks_msec() - elapsed > 100:
+		elapsed = OS.get_ticks_msec()
+		f = rand_range(0.0007, 0.008)
+		if name == "npc2":
+			f -= 0.0002
+
