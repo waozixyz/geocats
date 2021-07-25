7a83c966f67e415be9f95002b889a47b5b8c7a60
wolfi <wolfi@disroot.org>
2021-07-25 13:57:50 +0200

    add extra chat logic for affogato

--- Scenes/Chat.gd
+++ Scenes/Chat.gd
@@ -5,17 +5,26 @@ onready var chat_with = get_tree().get_current_scene().get_node("Default/CanvasL
 
 var active : bool
 var disabled : bool
-
+var parent_name : String
+var convo_file : String
 func _ready():
 	connect("body_entered", self, "_on_body_entered")
 	connect("body_exited", self, "_on_body_exited")
 
+func _get_complex_convo():
+	var scene_name = get_tree().get_current_scene().name
+	return parent_name + "_" + scene_name
 
 func show_chat():
+	parent_name = get_parent().name
+	if parent_name == "Affogato":
+		convo_file = _get_complex_convo()
+	else:
+		convo_file = parent_name
 	active = true
 	chat_with.hide_after = false
 	chat_with.visible = true
-	chat_with.name_label.text = get_parent().name
+	chat_with.name_label.text = parent_name
 	
 func hide_chat():
 	chat_with.visible = false
@@ -42,4 +51,5 @@ func _process(delta):
 func _input(event):
 	if active:
 		if Input.is_action_just_pressed("interact"):
-			chat_with.start(get_parent().name)
+			print(convo_file)
+			chat_with.start(convo_file)
