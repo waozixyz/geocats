4bd63041e0eb62277be99fbeb1ce53ead5d8e502
wolfi <wolfi@disroot.org>
2021-07-31 15:57:32 +0200

    add cave teleport

---
+++ Scenes/6_Creek/Interact.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+onready var button = $Button
+onready var sound = $Sound
+onready var player =  get_tree().get_current_scene().get_node("Default/Player")
+onready var chat_with =  get_tree().get_current_scene().get_node("Default/CanvasLayer/ChatWith")
+
+func _ready():
+	connect("body_entered", self, "_on_body_entered")
+	connect("body_exited", self, "_on_body_exited")
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		button.visible = true
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		button.visible = false
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_down") && button.visible == true && not chat_with.started:
+		if name == "CaveToDown":
+			player.position.x = 4420
+			player.position.y = 4950
+		elif name == "CaveToUp":
+			player.position.x = 4350
+			player.position.y = 3250
+
+
