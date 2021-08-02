b132fb96820a536ee1c54c4c3c1a9145828992a4
wolfi <wolfi@disroot.org>
2021-08-02 18:54:26 +0200

    add geo nav highlights

--- Scenes/UI/ButtonArea.gd
+++ Scenes/UI/ButtonArea.gd
@@ -1,13 +1,21 @@
 extends Area2D
 
 var pressed : bool = false
-
+var hovered: bool = false
 func _ready():
 	connect("input_event", self, "_on_input_event")
+	connect("mouse_entered", self, "_on_mouse_entered")
+	connect("mouse_exited", self, "_on_mouse_exited")
 	set_process_input(true)
 
-		
 func _on_input_event( viewport, event, shape_idx ):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
 			pressed = true
+
+func _on_mouse_entered():
+	hovered = true
+			
+func _on_mouse_exited():
+	hovered = false
+	
--- Scenes/UI/Feline.gd
+++ Scenes/UI/Feline.gd
@@ -3,10 +3,29 @@ extends CanvasLayer
 onready var base = $Base
 onready var eyes = $Base/Eyes
 onready var icons = $Base/Icons
+
+## buttons
+onready var bt_exit = icons.get_node("Exit")
+onready var bt_map = icons.get_node("Map")
+onready var bt_location = icons.get_node("Location")
+onready var bt_return = icons.get_node("Return")
+
 var active : bool = false
 
 func _ready():
 	pass
+
+func check(button, action):
+	var rect = button.get_node("ColorRect")
+	var area = button.get_node("Area2D")
+	if area.pressed:
+		area.pressed = false
+		if action == "exit":
+			active = false
+	if area.hovered:
+		rect.visible = true
+	else:
+		rect.visible = false
 
 var ticks = 0
 func _process(delta):
@@ -15,16 +34,26 @@ func _process(delta):
 			eyes.frame = 2
 		if ticks == 10:
 			eyes.frame = 1
+		## make icons visible
 		if ticks > 15:
 			eyes.visible = false
 			icons.visible = true
+			
+			# exit button
+			check(bt_exit, "exit")
+			check(bt_map, "map")
+			check(bt_location, "location")
+			check(bt_return, "menu")
+
 		else:
 			icons.visible = false
 			eyes.visible = true
+
 		if ticks < 30:
 			ticks += .5
 		base.visible = true
 	else:
+		# start shutdown and hide feline
 		base.visible = false
 		if ticks > 0:
 			if ticks == 14:
