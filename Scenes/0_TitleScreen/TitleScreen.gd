2dc11897fa43252341ec441b9e06e468e4f2d710
wolfi <wolfi@disroot.org>
2021-08-05 13:15:30 +0200

    add slideshow

--- Scenes/0_TitleScreen/TitleScreen.gd
+++ Scenes/0_TitleScreen/TitleScreen.gd
@@ -5,5 +5,5 @@ func _ready():
 	play_button.connect("pressed", self, "_start_game")
 
 func _start_game():
-	SceneChanger.change_scene("res://Scenes/1_CatCradle/1_CatCradle.tscn", 0, "", 1)
+	SceneChanger.change_scene("CatCradle", 0, "", 1)
 
