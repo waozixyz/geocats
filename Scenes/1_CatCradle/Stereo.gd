34c082342126e03182f16091295dc8d83339535e
kurtis <geocats@protonmail.com>
2021-08-08 11:45:38 -0600

    added AudioStreamManager.gd and Stereo.gd

---
+++ Scenes/1_CatCradle/Stereo.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+onready var player =  get_tree().get_current_scene().get_node("Default/Player")
+var disabled: bool = false
+var available = []
+func _ready():
+	assert(connect("body_entered", self, "_on_body_entered") == 0)
+	assert(connect("body_exited", self, "_on_body_exited") == 0)
+
+
+func _on_body_entered(body):
+	if body.name == "Player" and not disabled:
+		AudioStreamManager.play("res://Assets/Sfx/Transition/WayoWayo.ogg")
