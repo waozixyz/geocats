925266e1bac8347d46ee982952061a9ff1cf53ef
kurtis <geocats@protonmail.com>
2021-08-09 02:56:03 -0600

    Added SFX, gave complex characters a voice

--- Scenes/2_Complex/Cleopardis.gd
+++ Scenes/2_Complex/Cleopardis.gd
@@ -3,7 +3,7 @@ extends Entity
 var anim = "idle"
 
 func _ready():
-	sprite = $ViewportContainer/Viewport/AnimatedSprite
+	sprite = $ViewportContainer/Viewport/AnimatedSprite2
 	to_rotate = $ViewportContainer
 	._ready()
 
