33af1d03541f8c6474662d30a352f0c1cdead68a
Wolfi <wolfi@disroot.org>
2021-09-04 00:55:15 +0200

    add new telescope logic

--- Scenes/Levels/8_GeoLodge/Fire.gd
+++ Scenes/Levels/8_GeoLodge/Fire.gd
@@ -1,14 +1,7 @@
 extends InteractSimple
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 onready var sprite = $AnimatedSprite
 
-
-# Called when the node enters the scene tree for the first time.
 func _process(delta):
 	._process(delta)
 	if do_something:
