8e4a4bf0209b3c1166115d10b4f34e4b59dab7c0
Wolfi <wolfi@disroot.org>
2021-09-03 22:50:02 +0200

    improve geolodge

--- Scenes/Levels/8_GeoLodge/interactions.gd
+++ Scenes/Levels/8_GeoLodge/interactions.gd
@@ -4,6 +4,6 @@ extends InteractButton
 func _input(event):
 	if _can_interact():
 		object.visible = false
-		if name == "GoToCity":
-			SceneChanger.change_scene("GeoCity", 2, "WoodDoorLatchOpen1", 1.5)
+		if name == "GoToCaves":
+			SceneChanger.change_scene("Caves", 0, "WoodDoorLatchOpen1", 1.5)
 
--- Scenes/Levels/9_Caves/interactions.gd
+++ Scenes/Levels/9_Caves/interactions.gd
@@ -4,6 +4,6 @@ extends InteractButton
 func _input(event):
 	if _can_interact():
 		object.visible = false
-		if name == "GoToCity":
-			SceneChanger.change_scene("GeoCity", 2, "WoodDoorLatchOpen1", 1.5)
+		if name == "GoToLodge":
+			SceneChanger.change_scene("GeoLodge", 2, "WoodDoorLatchOpen1", 1.5)
 
