2920a5d448e72909d47b5997e706f7e50423c86d
kurtis <geocats@protonmail.com>
2021-10-19 00:01:42 +0200

    very basic theme checkpoint

--- Scenes/Levels/3_GeoCity/3_GeoCity.gd
+++ Scenes/Levels/3_GeoCity/3_GeoCity.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
 onready var chat_with = get_tree().get_current_scene().get_node("Default/CanvasLayer/ChatWith")
+onready var below_player = $BelowPlayer
+
+var theme = "Creepy_City"
+var theme_path = "res://Assets/Levels/3_GeoCity/" + theme + "/"
+
+func _ready(): #Change to Pumpkin function which is called by collision
+	below_player.get_node("City_Buildings").texture = load(theme_path + "bg.png")
+	below_player.get_node("City_BG").texture = load(theme_path + "red_sky.png")
 
 func _physics_process(delta):
 	var teleport = PROGRESS.variables.get("teleport_geolodge")
