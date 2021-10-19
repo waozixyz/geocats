38c764a209afc515ebfb2565552e2283ad400840
kurtis <geocats@protonmail.com>
2021-10-19 18:22:15 +0200

    Added dialogue to cave level

--- Scenes/Levels/3_GeoCity/3_GeoCity.gd
+++ Scenes/Levels/3_GeoCity/3_GeoCity.gd
@@ -7,7 +7,7 @@ onready var above_player = $FrontPlayer
 var theme = "Creepy_City"
 var theme_path = "res://Assets/Levels/3_GeoCity/" + theme + "/"
 
-func _ready(): #Change to Pumpkin function which is called by collision
+func _spooky_theme(): #Change to Pumpkin function which is called by collision
 	below_player.get_node("City_Buildings").texture = load(theme_path + "bg.png")
 	below_player.get_node("City_BG").texture = load(theme_path + "red_sky.png")
 	below_player.get_node("Moon").visible = true
@@ -17,6 +17,9 @@ func _ready(): #Change to Pumpkin function which is called by collision
 	below_player.get_node("Pumpkin_Small").visible = true
 	below_player.get_node("Creepy_Cat").visible = true
 	above_player.get_node("Giant_Pumpkin").play("Happy")
+	get_node("Salty_Swing").stop()
+	get_node("Creepy_Swing").play()
+
 func _physics_process(delta):
 	var teleport = PROGRESS.variables.get("teleport_geolodge")
 
