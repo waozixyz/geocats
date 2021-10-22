4c0873e260d746ea3a27e29e096e057028ac5f67
kurtis <geocats@protonmail.com>
2021-10-22 14:46:18 +0200

    Safety commit, for the merge lol

--- Scenes/Levels/3_GeoCity/3_GeoCity.gd
+++ Scenes/Levels/3_GeoCity/3_GeoCity.gd
@@ -7,7 +7,7 @@ onready var above_player = $FrontPlayer
 var theme = "Creepy_City"
 var theme_path = "res://Assets/Levels/3_GeoCity/" + theme + "/"
 
-func _spooky_theme(): #Change to Pumpkin function which is called by collision
+func _ready(): #Change to Pumpkin function which is called by collision
 	below_player.get_node("City_Buildings").texture = load(theme_path + "bg.png")
 	below_player.get_node("City_BG").texture = load(theme_path + "red_sky.png")
 	below_player.get_node("Moon").visible = true
