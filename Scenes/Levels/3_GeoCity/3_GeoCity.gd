c8280cb5a5ba17e425cdebff0a91c5649d58c8fd
kurtis <geocats@protonmail.com>
2021-12-06 14:15:19 +0100

    wassup

--- Scenes/Levels/3_GeoCity/3_GeoCity.gd
+++ Scenes/Levels/3_GeoCity/3_GeoCity.gd
@@ -5,30 +5,30 @@ onready var below_player = $BehindPlayer
 onready var above_player = $FrontPlayer
 onready var giant_pumpkin = $GiantPumpkin
 
-var theme = "GeoCity"
-var res_path = "res://Assets/Levels/3_GeoCity/"
-var last_theme = "GeoCity"
+var theme = "CreepyCity"
+var snow_theme = "SnowyCity"
+
+var theme_path = "res://Assets/Levels/3_GeoCity/" + theme + "/"
+var snow_theme_path = "res://Assets/Levels/3_GeoCity/" + snow_theme + "/"
 func _ready():
 	giant_pumpkin.visible = true
 
-
-func set_theme(): #Change to Pumpkin function which is called by collision
-	if has_node("BehindPlayer/" + theme):
-		get_node("BehindPlayer/" + theme).visible = true
-	if has_node("AbovePlayer/" + theme):
-		get_node("AbovePlayer/" + theme).visible = true
-
-	below_player.get_node("City_Buildings").texture = load(res_path + theme + "/bg.png")
-	below_player.get_node("City_BG").texture = load(res_path + theme + "/red_sky.png")
-	get_node("Music/Salty_Swing").stop()
-	get_node("Music/Creepy_Swing").play()
+func snowy_city(): #Change to Pumpkin function which is called by collision
+	if has_node(snow_theme):
+		snow_theme.visible = true
+	below_player.get_node("City_Buildings").texture = load(theme_path + "bg.png")
+	get_node("Salty_Swing").stop()
+	get_node("Snowy_Swing").play()
+	
+func creepy_city(): #Change to Pumpkin function which is called by collision
+	if has_node(theme):
+		theme.visible = true
+	below_player.get_node("City_Buildings").texture = load(theme_path + "bg.png")
+	below_player.get_node("City_BG").texture = load(theme_path + "red_sky.png")
+	get_node("Salty_Swing").stop()
+	get_node("Creepy_Swing").play()
 
 func _physics_process(delta):
-	if PROGRESS.variables.get("NonacoPumpkinPuzzle"):
-		theme = "CreepyCity"
-	if theme != "GeoCity" and last_theme != theme:
-		set_theme()
-		last_theme = theme
 	var teleport = PROGRESS.variables.get("teleport_geolodge")
 
 	if teleport:
