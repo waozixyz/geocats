ba2747ff653d88691c941f34ccfea2322cb36f9b
wolfi <wolfi@disroot.org>
2022-01-16 16:33:37 +0100

    fix buttons
    Former-commit-id: 0260a6cf1526c556b8e1c584f2956ea44be3c6f4

---
+++ Scenes/Levels/10_DesertOutpost/ComputerTextLabel.gd
@@ -0,0 +1,15 @@
+extends RichTextLabel
+
+onready var file = 'res://Assets/Levels/10_DesertOutpost/survival_guide/weather.txt'
+onready var f = File.new()
+func _on_Button_toggled(button_pressed):
+	if button_pressed==true:
+		if f.file_exists(file):
+			f.open(file, File.READ)
+			while not f.eof_reached(): # iterate through all lines until the end of file is reached
+				var line = f.get_line()
+				text += line
+				newline()
+			f.close()
+	elif button_pressed==false:
+			print('goodbye') # hide popup
