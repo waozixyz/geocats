305452e2dc9d4bc315738f8fc51fc6b13a225c43
wolfi <wolfi@disroot.org>
2022-03-04 22:05:18 +0100

    start fixing geolodge
    Former-commit-id: b176db9122572fa40b0b0f3e0b1b344abbfd805f

--- Scenes/UI/Teleport/External.gd
+++ Scenes/UI/Teleport/External.gd
@@ -4,7 +4,8 @@ class_name TeleportExternal
 enum Territory {
 	GeoCity
 	Geoterra
-	Geodesert
+	Glaciokarst
+	XAPS
 	Geoquarium
 }
 
@@ -25,16 +26,32 @@ enum Geoterra {
 	GeoCacheRoom
 	Mountain
 }
+
+enum Glaciokarst {
+	GeoLodge
+	Caves
+	Battle
+}
+
+enum XAPS {
+	DesertOutpost
+}
+
+
 export(Territory) var territory
 export(GeoCity) var geocity
 export(Geoterra) var geoterra
+export(Glaciokarst) var glaciokarst
+export(XAPS) var xaps
 export(int) var location = 0
 
 func _get_level_name():
 	match territory:
 		Territory.GeoCity: return GeoCity.keys()[geocity]
 		Territory.Geoterra: return Geoterra.keys()[geoterra]
-
+		Territory.Glaciokarst: return Glaciokarst.keys()[glaciokarst]
+		Territory.XAPS: return XAPS.keys()[xaps]
+		
 var territory_name
 var level_name
 func _ready():
