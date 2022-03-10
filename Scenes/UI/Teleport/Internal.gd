4de86c9d1be27cbd04545a236bebc240143b8669
wolfi <wolfi@disroot.org>
2022-03-10 12:18:32 +0100

    improve teleporter system
    Former-commit-id: 93cb436e288a18dfeeb62cf17ea255cc0a18b21c

--- Scenes/UI/Teleport/Internal.gd
+++ Scenes/UI/Teleport/Internal.gd
@@ -4,31 +4,18 @@ class_name TeleportInternal
 onready var affogato =  get_tree().get_current_scene().get_node("Default/Affogato")
 onready var chat_with = get_tree().get_current_scene().get_node("Default/CanvasLayer/ChatWith")
 
-export(Vector2) var new_position 
+export(NodePath) var node_to_go 
 
 var teleporting
-var dest_x
-var dest_y
+var to_go
 
-var tween = Tween.new()
+var tween
 func _ready():
-	add_child(tween)
+	if node_to_go:
+		to_go = get_node(node_to_go)
 	._ready()
 	
-func _teleport(new_pos: Vector2):
-	player.visible = false
-	player.disable("teleport")
-
-	teleporting = true
-
-	tween.interpolate_property(player, "position",
-		player.position, new_pos, 1,
-		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	tween.start()
-
 func _process(_delta):
-	if teleporting:
-		chat_with.visible = false
 	if teleporting and not tween.is_active():
 		player.visible = true
 		player.enable("teleport")
@@ -36,11 +23,13 @@ func _process(_delta):
 		if player.followers:
 			for follower in player.followers:
 				follower.position = Vector2(0,0)
-			print(player.followers)
-			#affogato.position = player.position
 
 func _input(_event):
 	if _can_interact():
 		_play_sound()
-
-		_teleport(new_position)
+		if to_go and not teleporting:
+			teleporting = true
+			player.disable("teleport")
+			chat_with.visible = false
+			player.visible = false
+			tween = utils.tween_position(player, to_go.position)
