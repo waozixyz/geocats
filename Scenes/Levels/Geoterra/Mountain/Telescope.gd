4b79eb4b854fbe5f2352b991d6ab45d3a2659ae8
kurtis <geocats@protonmail.com>
2022-03-17 13:51:58 +0100

    telescope in mountain
    Former-commit-id: a2e36f05275d3966e1cbdae3c503c22eedc7775c

--- Scenes/Levels/Geoterra/Mountain/Telescope.gd
+++ Scenes/Levels/Geoterra/Mountain/Telescope.gd
@@ -12,18 +12,15 @@ func _ready():
 var active_sound
 func _process(_delta): 
 	if do_something and camera:
-		if camera.modulate.a == 1:
-			utils.tween(camera, "zoom", Vector2(9, 9), 2)
+		if camera.zoom.x == 1.5:
+			utils.tween(camera, "zoom", Vector2(9, 9), 1)
 			timer = 0
-			if active_sound:
-				active_sound.stop()
-			if not disable_player.empty():
-				current_scene.set_disable("player", disable_player, false)
-				
 		else:
-			utils.tween(camera, "zoom", Vector2(0, 0), 2)
+			utils.tween(camera, "zoom", Vector2(1.5, 1.5), 1)
 			if sound_when_visible:
 				active_sound = AudioManager.play_sound(sound_when_visible, sound_volume, true)
+			if not disable_player.empty():
+				current_scene.set_disable("player", disable_player, false)
 			timer = 0
 		do_something = false
 
