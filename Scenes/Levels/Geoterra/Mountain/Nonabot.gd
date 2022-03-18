e8dff7b85e3a6e35ddfad20687f17acc68c53197
wolfi <wolfi@disroot.org>
2022-03-18 19:56:37 +0100

    add shopkeeper likn
    Former-commit-id: 56bd9d0488ba7ed403d649ed6638017a06360ff1

--- Scenes/Levels/Geoterra/Mountain/Nonabot.gd
+++ Scenes/Levels/Geoterra/Mountain/Nonabot.gd
@@ -2,9 +2,6 @@ extends Node2D
 
 
 onready var chat_npc = $Area2D
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
