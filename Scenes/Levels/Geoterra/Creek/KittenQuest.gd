74098a9378033b8030c2a022c16a3c3e04bb421e
wolfi <wolfi@disroot.org>
2022-04-01 21:57:50 +0200

    hide teacher quest kittens if quest already complete
    Former-commit-id: d4a444228ee2fe82401a40f3be0a5a8104893fbb

--- Scenes/Levels/Geoterra/Creek/KittenQuest.gd
+++ Scenes/Levels/Geoterra/Creek/KittenQuest.gd
@@ -15,6 +15,11 @@ var left_bound = 0
 var right_bound = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	PROGRESS.variables["geoterra_kitten_found_them"] = true
+	if PROGRESS.variables.has("geoterra_kitten_found_them") and PROGRESS.variables["geoterra_kitten_found_them"]:
+		visible = false
+		for child in get_children():
+			remove_child(child)
 	var left_bound = collision_shape.position.x - collision_shape.shape.extents.x
 	var right_bound = collision_shape.position.x + collision_shape.shape.extents.x
 	for child in get_children():
